efa0102b61becf58d04952fdf95765f8026a660b
JoyH127 <hh495@scarletmail.rutgers.edu>
2022-02-02 16:07:50 -0500

    completed lab-hee

--- main.js
+++ main.js
@@ -12,15 +12,37 @@ const carBrands = [
   { brand: "Nissan", model: "frontier", type: "pickup" },
 ];
 
+const sedans = carBrands.filter(car=> car.type === "sedan");
+console.log(sedans);
+
 /* 2.
  * reverseString takes a string
  * and should return the reverse of the string, you cannot use .reverse method
  * e.g., reverseString('cat') => 'tac'
  */
 
+function reverse(str){
+  // take an argument from input
+  // set const "rev" as an empty string to save reverse string in an order.
+  const rev =[];
+  // loop all characters in an original string. it will start from end to start. 
+  for(let i = str.length-1; i >= 0; i--){
+    // push the last element to first element.
+    rev.push(str[i]);
+  } 
+  return rev.join('');
+}
+
+console.log(reverse("cat"));
+
+
 // 3.
 // Write a function that takes an array of strings,
 // and returns the longest string in the array
+
+const arrString = (arr) => {return arr.join(" ");}
+
+console.log(arrString(["hey!","Little","monster!","I","will","catch","you!"]));
 
 // 4.
 // Using Reduce,
@@ -36,10 +58,11 @@ let wishlist = [
   { title: "tesla", price: 2000 },
   { title: "tesla", price: 90000 },
 ];
-function shop(arr) {}
+function shop(arr) {
+  const total = arr.reduce((acc,cur)=> acc +cur.price,0);
+  return total;
+}
 console.log(shop(wishlist));
-
-
 
 
 //5. OOP has 4 pillars and we learned about each in this phase. 
@@ -47,6 +70,8 @@ console.log(shop(wishlist));
 //and explain it in simple terms with an example.
 //Also, if you cannot explain any of them, we have failed as 
 //instructors and you will be banished to the nether realms.
+
+// Encapsulation means the action of enclosing something in or as if in a capsule. 
 
 
 /*  6.
@@ -59,7 +84,11 @@ console.log(shop(wishlist));
  * then return the new array
  * Be sure to use map()!
  */
-const flipBool = (arr) => {};
+const flipBool = (arr) => {
+  const flip = arr.map((bool)=> {return !bool;});
+  return flip;
+}
+console.log(flipBool([false,true,true,false,false,false]));
 
 // 7.
 //FEAST OR FAMINE
@@ -69,3 +98,20 @@ const flipBool = (arr) => {};
 // > i.e.:
 // > input => `"great blue heron", "garlic naan"`
 // > output => `"gn"`
+
+
+function match(a,b){
+  const animal = a.toLowerCase();
+  const food = b.toLowerCase();
+  const match = []; 
+  if(animal< 2 || food <2){
+    console.log("type at least two letters each.")
+  }
+  else if(animal[0]==food[0] && animal[animal.length-1]==food[food.length-1]){
+    match.push(animal[0]+food[food.length-1]);
+    return match
+  }
+  return false;
+}
+
+console.log(match("great blue heron", "garlic naan"));
